a5fad796fdc9e54dc4512f8dfae1111db0de393a
AlexRDN8 <alexrodin8@bk.ru>
2022-12-05 16:34:50 +0300

    added task3

---
+++ task3/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N. 
+Задание должно быть выполнено в методе и метод должен вернуть массив чисел.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+Console.WriteLine("Введите число: ");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int N);
+int[] squareOfNumber = getSquareOfNumber(N);
+PrintArray(squareOfNumber);
+
+
+
+if (!isParsedN)
+{
+    Console.WriteLine("Должно быть введено число");
+    return;
+}
+
+int[] getSquareOfNumber(int N)
+{
+    int[] array = new int[N];
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        array[index] = (int)(Math.Pow(index+1, 3));
+        index ++;               
+    }
+    return array;         
+}
+
+void PrintArray (int [] array)
+{
+    for (int index = 0; index < array.Length; index ++)
+    {
+        Console.WriteLine($"{array[index]} ");
+    }
+    Console.WriteLine();
+}
